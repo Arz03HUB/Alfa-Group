11fc1efd89c7c5b7d9bfd82b3c8c27f2b7b52150
Arz03HUB <ArzziioX0X@outlook.com>
2026-08-17 14:53:33 -0400

    first init
    init()

---
+++ Group2Alfa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Group2Alfa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
